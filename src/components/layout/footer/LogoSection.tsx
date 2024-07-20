7ef8fff3ed90a1e67e12d3efae95f30ee4ee65c6
AhmedAmin <ahmedamin01147@hotmail.com>
2024-07-21 00:09:58 +0300

    update-footer

--- src/components/layout/footer/LogoSection.tsx
+++ src/components/layout/footer/LogoSection.tsx
@@ -6,7 +6,7 @@ import { GiBirdTwitter } from "react-icons/gi";
 import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 import cn from "@/utils/cn";
 const LogoSection = () => {
-    const iconSize = 20;
+  const iconSize = 20;
   return (
     <div className="w-full md:w-1/3 flex flex-col gap-y-[20px]">
       <Image src={logo} loading="lazy" width={300} height={200} alt="logo" />
@@ -14,29 +14,37 @@ const LogoSection = () => {
         In the new era of technology we look a in the future with certainty and
         pride to for our company and.
       </article>
-      <div className="flex flex-row gap-[10px]">
-        <SocialLink href="" children={<FaTwitter size={iconSize}/>} />
-        <SocialLink href="" children={<FaFacebook size={iconSize}/>} />
-        <SocialLink href="" children={<FaInstagram size={iconSize}/>} />
-        <SocialLink href="" children={<FaGithub size={iconSize}/>} />
-      </div>
+      <SocialLink href="">
+        <FaTwitter size={iconSize} />
+      </SocialLink>
+      <SocialLink href="">
+        <FaFacebook size={iconSize} />
+      </SocialLink>
+      <SocialLink href="">
+        <FaInstagram size={iconSize} />
+      </SocialLink>
+      <SocialLink href="">
+        <FaGithub size={iconSize} />
+      </SocialLink>
     </div>
   );
 };
 
 export default LogoSection;
 
-interface TSocialLinkProps{
-    href: string;
-    children: React.ReactNode;
-    className?: string
+interface TSocialLinkProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-
-const SocialLink = (props : TSocialLinkProps)=>{
-    return(
-        <Link href={props.href} className={cn("bg-red text-[#fff] p-3 rounded-full", props.className)}>
-            {props.children}
-        </Link>
-    )
-}
+const SocialLink = (props: TSocialLinkProps) => {
+  return (
+    <Link
+      href={props.href}
+      className={cn("bg-red text-[#fff] p-3 rounded-full", props.className)}
+    >
+      {props.children}
+    </Link>
+  );
+};
