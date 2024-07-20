935526574ca8eee373493b0dddafcd396eae7913
AhmedAmin <ahmedamin01147@hotmail.com>
2024-07-21 01:36:57 +0300

    update-footer

--- src/components/layout/footer/InstagramSection.tsx
+++ src/components/layout/footer/InstagramSection.tsx
@@ -7,7 +7,7 @@ const InstagramSection = () => {
   return (
     <div className='w-full flex  flex-col gap-[20px] md:w-1/3'>
       <h3>Follow Us On Instagram</h3>
-      <Link href={""} className="flex flex-wrap flex-row w-full gap-[10px]">
+      <Link href={""} className="flex  flex-wrap flex-row w-full gap-[10px]">
       
       {imagesArray.map(item=><LinkImage key={item.id} src={item.src} />)}
       </Link>
@@ -26,6 +26,6 @@ interface TLinkImageProps{
 }
 
 const LinkImage = (props: TLinkImageProps)=>{
-    return <Image loading='lazy' className='rounded-lg hover:blur-[1px] hover:shadow-lg transition-all duration-[750]' src={props.src} width={props.width || 200} height={props.height || 200} alt={props.alt||"image"}></Image>
+    return <Image loading='lazy' className='w-[45%] rounded-lg hover:blur-[1px] hover:shadow-lg transition-all duration-[750]' src={props.src} width={props.width || 200} height={props.height || 200} alt={props.alt||"image"}></Image>
   
 }
