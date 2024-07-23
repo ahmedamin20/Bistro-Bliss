e125ba800ee155a6d31b94e6d5f41ac306f9207f
AhmedAmin <ahmedamin01147@hotmail.com>
2024-07-23 17:19:22 +0300

    offer-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/pages/Home/Banner";
 import HomeContact from "@/components/pages/Home/HomeContact";
+import OffersSection from "@/components/pages/Home/OffersSection";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="w-full h-full overflow-x-hidden">
       <Banner />
       <HomeContact />
+      <OffersSection />
     </div>
   );
 }
