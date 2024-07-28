5f41450d41723b2f6525be1255bce7284a4e2fd6
AhmedAmin <ahmedamin01147@hotmail.com>
2024-07-28 03:31:36 +0300

    video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import Banner from '@/components/pages/Home/Banner';
 import HomeContact from '@/components/pages/Home/HomeContact';
 import OffersSection from '@/components/pages/Home/OffersSection';
-import dynamic from 'next/dynamic';
 
-// const Banner = dynamic(() => import('@/components/pages/Home/Banner'), { ssr: false });
-// const HomeContact = dynamic(() => import('@/components/pages/Home/HomeContact'), { ssr: false });
-// const OffersSection = dynamic(() => import('@/components/pages/Home/OffersSection'), { ssr: false });
 
 export default function Home() {
   return (
--- src/components/pages/About/VideoSection.tsx
+++ src/components/pages/About/VideoSection.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useRef, useState } from "react";
 import { FaPlay } from "react-icons/fa";
-
+import menu from "@/assets/about/menu.svg";
+import group from "@/assets/about/Group.svg";
+import timer from "@/assets/about/timer.svg";
+import Image, { StaticImageData } from "next/image";
 const src = "/videos/video.mp4";
 const VideoSection = () => {
   const [play, setPlay] = useState(false);
@@ -17,7 +20,7 @@ const VideoSection = () => {
   };
 
   return (
-    <div className="w-full my-4">
+    <div className="w-full my-4 flex flex-col">
       <div className="relative h-[500px] w-full">
         <video
           onClick={handleClick}
@@ -32,14 +35,55 @@ const VideoSection = () => {
             className="top-0 flex flex-col justify-center gap-[20px] items-center bg-black/60 w-full h-full absolute"
           >
             <span className="p-[2rem] rounded-full flex bg-white justify-center items-center">
-            <FaPlay className="text-red cursor-pointer" size={15} />
+              <FaPlay className="text-red cursor-pointer" size={15} />
             </span>
-            <span className="font-roboto text-white font-bold text-[45px] w-full md:w-[30%]"> Feel the authentic & original taste from us</span>
+            <span className="font-roboto text-white font-bold text-[45px] w-full md:w-[30%]">
+              {" "}
+              Feel the authentic & original taste from us
+            </span>
           </button>
         )}
+      </div>
+      <div className="bg-white flex justify-evenly p-5 items-center flex-row flex-wrap w-full">
+        <Item
+          image={menu}
+          title="Multi Cuisine"
+          description={`In the new era of technology we look in
+          the future with certainty life.`}
+        />
+        <Item
+          image={group}
+          title="Easy To Order"
+          description={`In the new era of technology we look in
+          the future with certainty life.`}
+        />
+        <Item
+          image={timer}
+          title="Fast Delivery"
+          description={`In the new era of technology we look in
+          the future with certainty life.`}
+        />
       </div>
     </div>
   );
 };
 
 export default VideoSection;
+interface TItemProps {
+  title: string;
+  description: string;
+  image: StaticImageData;
+}
+const Item = (props: TItemProps) => {
+  return (
+    <div className="text-black my-4 font-bold text-[20px] w-[95%] md:w-[25%] gap-[20px] flex justify-evenly items-center flex-row">
+      <Image src={props.image} width={80} height={80} alt="" />
+      <span className="flex flex-col gap-[10px]">
+        <span className="font-[700] text-[15px] font-sans">{props.title}</span>
+        <span className="font-[500] text-grey-dark text-[12px] font-sans">
+          {props.description}
+        </span>
+      </span>
+    </div>
+  );
+};
