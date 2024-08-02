c6ae133b207937ff734d63a3802b8bce8bf8bc12
AhmedAmin <ahmedamin01147@hotmail.com>
2024-08-02 19:09:23 +0300

    fix-menu

--- src/components/pages/Menu/MenuSection.tsx
+++ src/components/pages/Menu/MenuSection.tsx
@@ -10,7 +10,7 @@ const MenuSection = () => {
   const [filter, setFilter] = useState(0);
   return (
     <div className='flex flex-col w-full gap-[20px]'>
-      <div className='flex flex-row justify-center gap-[20px] items-center mx-auto'>
+      <div className='flex flex-row flex-wrap justify-center gap-[20px] items-center mx-auto'>
         {filterData.map(item =>  (<CustomButton key={item.id} className={`${item.id == filter && "bg-red text-white"} px-8`} onClick={()=>setFilter(item.id)} title={item.name} />))}
       </div>
       <div className='w-full md:w-[85%] mx-auto flex flex-col md:flex-row p-9 flex-wrap justify-evenly items-center'>
